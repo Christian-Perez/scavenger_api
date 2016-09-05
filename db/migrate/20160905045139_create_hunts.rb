75aa3b5471ee6edacd47277c3627bf978bab0878
Christian Perez <chris.dillon92@gmail.com>
2016-09-05 13:24:05 -0700

    before first seed, preserve structure

--- db/migrate/20160905045139_create_hunts.rb
+++ db/migrate/20160905045139_create_hunts.rb
@@ -2,8 +2,8 @@ class CreateHunts < ActiveRecord::Migration[5.0]
   def change
     create_table :hunts do |t|
       t.references :hunt_template, foreign_key: true
-      t.timestamp :start_time
-      t.timestamp :end_time
+      t.datetime :start_time
+      t.datetime :end_time
 
       t.timestamps
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,48 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+test_objectives = Objective.create([
+  {name: 'find the marker',
+    description: 'say marker',
+    answer: 'marker',
+    completed: false},
+  {name: 'find the laptop charger',
+    description: 'minus the laptop',
+    answer: 'charger',
+    completed: false},
+  {name: 'find the backpack',
+    description: 'Type backpack',
+    answer: 'backpack',
+    completed: false},
+  {name: 'find the instructor',
+    description: 'MiIrKat starts with an "m" for ...',
+    answer: 'Mike',
+    completed: false},
+  {name: 'find the snack',
+    description: 'Santa Eats a single...',
+    answer: 'cookie',
+    completed: false},
+  {name: 'find the author of this program',
+    description: "What's my name...",
+    answer: 'Christian',
+    completed: false}
+  ])
+
+  # need to have multiple objectives
+  hunt_template_1 = HuntTemplate.create(
+    {name: 'Test Template 1',
+      description: 'The first Hunt Template :)',
+      objective: test_objectives}
+    )
+
+  player_1 = Player.create(name: 'Christian', email: 'chris@tian.com')
+
+  hunt_1 = Hunt.create(hunt_template: hunt_template_1, player: player_1 ,start_time: DateTime.now, end_time: 2016-09-20 10:30:00)
+
+# players = Player.create([
+#   {},
+#   {},
+#   {}
+#   ])
